10510f7979c21a745c6efe0267022af6a6cc4567
Rodolfo-bps <rodolfo.velazquezbps@gmail.com>
2022-12-10 10:48:03 -0600

    permisos con htdocs

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,19 +1,6 @@
 <?php
 
 
-session_start();
-
-if (!isset($_SESSION['id_usuario'])) {
-    header("location: ../index.php");
-}
-
-$rol = $_SESSION['rol'];
-$id_usuario = $_SESSION["id_usuario"];
-$tipo_usuario = $_SESSION["tipo_usuario"];
-$nombre_usuario = $_SESSION['nombre_usuario'];
-$nombre = $_SESSION['nombre'];
-
-
 //Conexion
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
---
+++ error404.php
@@ -0,0 +1,38 @@
+<?php include_once("template/head.php"); ?>
+
+<?php include_once("template/header.php"); ?>
+
+     
+  
+  
+  
+  
+  
+  
+  
+  
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+
+<!-- 404 Error Text -->
+<div class="text-center">
+    <div class="error mx-auto" data-text="404">404</div>
+    <p class="lead text-gray-800 mb-5">Page Not Found</p>
+    <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+    <a href="index.html">&larr; Back to Dashboard</a>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+
+
+
+<?php include_once("template/footer.php"); ?>
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
